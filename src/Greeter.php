9f4e2c96a0da91bd547d6204c728f5c8b9c96c96
hidenorigoto <hidenorigoto@gmail.com>
2018-07-23 21:36:27 +0900

    時計オブジェクトをGreeterのコンストラクタで受け取るように修正 [FAIL]

--- src/Greeter.php
+++ src/Greeter.php
@@ -3,6 +3,16 @@ namespace Hg\Greeter;
 
 class Greeter
 {
+    /**
+     * @var Clock
+     */
+    private $clock;
+
+    public function __construct(Clock $clock)
+    {
+        $this->clock = $clock;
+    }
+
     public function greet() :string
     {
         return 'おはようございます';
